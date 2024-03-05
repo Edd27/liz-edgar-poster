44639d5e30947d0cdf80eec721d1208f599b2a0a
Edgar Benavides <edgarben27@gmail.com>
2024-03-05 16:16:14 -0600

    feat: New songs

--- lib/data.ts
+++ lib/data.ts
@@ -1,12 +1,17 @@
 export const tracks = [
   {
-    title: "Ojitos de Miel",
-    src: "ojitos-de-miel.mp3",
-    author: "T3R Elemento",
-  },
-  {
     title: "A Donde Vamos",
     src: "a-donde-vamos.mp3",
     author: "Morat",
+  },
+  {
+    title: "Sparks",
+    src: "sparks.mp3",
+    author: "Coldplay",
+  },
+  {
+    title: "Ojitos de Miel",
+    src: "ojitos-de-miel.mp3",
+    author: "T3R Elemento",
   },
 ];
